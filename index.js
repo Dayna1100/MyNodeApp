00ed50618a08803586f850196e65142a2df7d576
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-06-27 14:44:30 -0400

    testing lodash

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ let name = "brittany";
 console.log("Name: " + name);
 
 // call the upperFirst() function in lodash to convert the first letter of the string to uppercase
-name = lodash.upperFirst(name);
+name = lodash.startCase(name);
 console.log("Revised name: " + name);
